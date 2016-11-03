599430ef93b1864521bce8328b1dd0fe1ab1d298
michael-lowe-nz <knfrances@gmail.com>
2016-11-04 12:18:37 +1300

    add showSong view in hbs

--- routes/spotify.js
+++ routes/spotify.js
@@ -7,17 +7,10 @@ var spotifyApi = new SpotifyWebApi()
 
 router.get('/', function(req, res){
   console.log("we're at the route")
-  res.render('index', {title: "Mel and Michael"})
+  res.render('showSong', {title: "Mel and Michael", songURI:songURI})
 })
-//
-// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-//   .then(function(data){
-//     console.log("Artist Albums", data.body.items[0])
-//   })
-//   .catch(function(err){
-//     console.log(err)
-//   })
-//
-// var songURL =  '<iframe src="https://embed.spotify.com/?uri=spotify:track:"'+songURI+'" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
-// var songURI = "64Ny7djQ6rNJspquof2KoX"
+
+var songURL =  '<iframe src="https://embed.spotify.com/?uri=spotify:track:"'+songURI+'" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>'
+var songURI = "64Ny7djQ6rNJspquof2KoX"
+
 module.exports = router
