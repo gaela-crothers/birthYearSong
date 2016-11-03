4f6828ae44cdbde26b59d3da44c141340fb46194
gabrielamelian <gabrielamelian@gamil.com>
2016-11-04 12:22:16 +1300

    Wrap song data in object

--- routes/spotifyGetSong.js
+++ routes/spotifyGetSong.js
@@ -12,10 +12,14 @@ spotifyApi.searchTracks('track:the year:' + year)
   .then(function(data) {
     var randomNum = Math.floor( Math.random() * (20) )
 
-    console.log('Search for random 2014 track with "the" - track name is', data.body.tracks.items[randomNum].name)
-    console.log('Search for random 2014 track with "the" - artist is', data.body.tracks.items[randomNum].artists[0].name)
-    console.log('Search for random 2014 track with "the" - album is', data.body.tracks.items[randomNum].album.name)
-    console.log('Search for random 2014 track with "the" - Spotify ID is', data.body.tracks.items[randomNum].id)
+    var songData = {
+      title: data.body.tracks.items[randomNum].name,
+      artist: data.body.tracks.items[randomNum].artists[0].name,
+      album: data.body.tracks.items[randomNum].album.name,
+      songURI: data.body.tracks.items[randomNum].id
+    }
+    
+    console.log(songData);
   }, function(err) {
     console.error(err)
   })
